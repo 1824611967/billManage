87b51d821dff03ff7a7a936ea264e1d4c100fda5
wanghongfeng <wanghongfeng@tt.com>
2018-12-29 10:41:47 +0800

    init billManage

---
+++ src/accessory/responseInfo.go
@@ -0,0 +1,29 @@
+package accessory
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct{
+	Code string 	 `json:"code"`
+	Msg  string 	 `json:"msg"`
+	Data interface{} `json:"data"`
+}
+func NewResponse() *Response{
+	return &Response{
+		Code: "default",
+		Msg : "default",
+	}
+}
+
+func (r *Response) Answer(w http.ResponseWriter) error{
+	if r.Code == "default"{
+		return nil
+	}else {
+		jsonStr, err := json.Marshal(r)
+		w.Write(jsonStr)
+
+		return err
+	}
+}
